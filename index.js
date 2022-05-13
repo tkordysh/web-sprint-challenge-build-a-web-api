bdf090e2c040eefb99688e8cc2a778df50a24f04
Tony Kordysh <tkordysh@gmail.com>
2022-05-13 10:28:29 -0400

    server running

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+
+const server = require('./api/server.js');
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+
