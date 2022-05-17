b11210e921b8a5bf66cfb373d5f882c55525f8f4
Tony Kordysh <tkordysh@gmail.com>
2022-05-17 18:57:46 -0500

    get functionality added to projects router

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,19 @@
 // add middlewares here related to projects
+const Project = require("./projects-model");
+const db = require("../../data/dbConfig");
+
+// exports.checkProjectPayload = (req, res, next)
+
+exports.checkProjectId = async (req, res, next) => {
+  try {
+    const project = await Project.get(req.params.id);
+    if (!project) {
+      next({ status: 404, message: "project not found" });
+    } else {
+      req.project = project;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,69 @@
 // Write your "projects" router here!
-const router = require('express').Router();
-const md = require('./projects-middleware');
-const Project = require('./projects-model');
+const router = require("express").Router();
+const md = require("./projects-middleware");
+const Project = require("./projects-model");
 
+//GET ALL
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Project.get();
+    res.json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
 
+//GET BY ID
+router.get("/:id", md.checkProjectId, async (req, res, next) => {
+  try {
+    const project = await Project.get(req.params.id);
+    res.json(project)
+  } catch (err) {
+    next(err);
+  }
+});
 
+// //POST
+// router.post("/", async (req, res, next) => {
+//   try {
+//     console.log("post project");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
+// //PUT
+// router.put("/:id", async (req, res, next) => {
+//   try {
+//     console.log("update project");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// //DELETE
+// router.delete("/:id", async (req, res, next) => {
+//   try {
+//     console.log("delete project");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// //GET BY ID AND ACTION
+// router.get("/", async (req, res, next) => {
+//   try {
+//     console.log("get by id and action");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// //Error handling
+// router.use((err, req, res, next) => {
+//   res.status(err.status || 500).json({
+//     message: err.message,
+//   });
+// });
 
 module.exports = router;
-
